5bfbfafb8de1099bee64336b7af3f250de86b53a
BLGFQY <54372768+BLGFQY@users.noreply.github.com>
2021-09-14 17:24:50 -0500

    finished functionalizing, fixed evaluation, minor algorithm improvements

--- main.py
+++ main.py
@@ -9,20 +9,10 @@ def evaluation(hand):
 
     score = 0
 
-    for i in range(1, 13):
-        # score primary player hand
-        if hand[i][0] == 1:
-            score += 4
-            print(+4)
-        if hand[i][0] == 13:
-            score += 3
-            print(+3)
-        if hand[i][0] == 12:
-            score += 2
-            print(+2)
-        if hand[i][0] == 11:
-            score += 1
-            print(+1)
+    # score player hand
+    for i in range(0, 13):
+        # adds points to the players score for Jacks, Queens, Kings, and Aces
+        if hand[i][0] > 10: score += (hand[i][0] % 10)
 
         # count number of each suit
         if hand[i][1] == "Clubs": distr[0] += 1
@@ -30,84 +20,39 @@ def evaluation(hand):
         if hand[i][1] == "Diamonds": distr[2] += 1
         if hand[i][1] == "Hearts": distr[3] += 1
 
-    # score number of suits
+    # score distribution of suits
     size = len(distr)
     for k in range(size):
-        if distr[k] == 0:
-            score += 5
-            print(+5)
-        if distr[k] == 1:
-            score += 2
-            print(+2)
-        if distr[k] == 2:
-            score += 1
-            print(+1)
+        if distr[k] == 0: score += 5
+        if distr[k] == 1: score += 2
+        if distr[k] == 2: score += 1
 
     return score
 
 
-print("Cards appear as follows:\n   1(Ace), 2, 3, 4, 5, 6, 7, 8, 9, 10, 11(Jack), 12(Queen), 13(King)\n")
- # Ace (1) (+4) | Jack (11) (+1) | Queen (12) (+2) | King (13) (+3) | doubleton (+1) | singleton (+2) | void (+5)
+print("\nCards appear as follows:\n    2, 3, 4, 5, 6, 7, 8, 9, 10, 11(Jack), 12(Queen), 13(King), 14(Ace)")
+# Jack (11) (+1) | Queen (12) (+2) | King (13) (+3) | Ace (14) (+4) | doubleton (+1) | singleton (+2) | void (+5)
 
 totalHands = 1000
 play = True
 
 while play:
-    # Build Deck
-    # this loop puts the deck together
+    # this loop builds the deck in the form of a 2d list
     suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
     deck = []
     for i in suits:
-        for j in range(1, 14):
+        for j in range(2, 15):
             deck.append([j, i])
     random.shuffle(deck)
 
-    # primary player hand variables
+    # player is dealt a hand and it's scored
     primary = deck[:13] # hold cards in hand
     deck = deck[13:]    # deletes primary hand from deck
     score = evaluation(primary)
 
-    # variable to hold number of each suit in a hand
-    #   Clubs, Spades, Diamonds, Hearts
-    # distr = [0, 0, 0, 0]
-    #
-    # # PRIMARY PLAYER
-    # for i in range(1, 14):
-    #
-    #     # create primary player hand
-    #     primary.append(deck[i])
-    #
-    #     # score primary player hand
-    #     if deck[i][0] == 1 : score += 4
-    #     if deck[i][0] == 13 : score += 3
-    #     if deck[i][0] == 12 : score += 2
-    #     if deck[i][0] == 11 : score += 1
-    #
-    #     # count number of each suit
-    #     if deck[i][1] == "Clubs" : distr[0] += 1
-    #     if deck[i][1] == "Spades" : distr[1] += 1
-    #     if deck[i][1] == "Diamonds" : distr[2] += 1
-    #     if deck[i][1] == "Hearts" : distr[3] += 1
-    #
-    #     # remove primary player hand from deck
-    #     deck.remove(deck[i])
-    #
-    # # score number of suits
-    # size = len(distr)
-    # for k in range(size):
-    #     if distr[k] == 0: score += 5
-    #     if distr[k] == 1: score += 2
-    #     if distr[k] == 2: score += 1
-
-
-    print("Here is your hand\n", primary)
+    print("\nHere is your hand\n", primary)
     print("This hand is worth", score, " points.")
     print("Running simulation.....")
-    #################################################
-
-    # secondary player hand variables
-    secondary = [] # holds card in hand
-    secondaryScore = 0
 
     # variable to calculate results
     # Pass, Part Score, Game, Small Slam, Grand Slam
@@ -115,38 +60,12 @@ while play:
 
     # SECONDARY PLAYER
     for i in range(0, totalHands):
-
-        #reset variables
-        distr = [0, 0, 0, 0]
-        secondaryScore = 0
-        secondary.clear()
-
-        # loop deals one hand
-        for j in range(1, 14):
-            random.shuffle(deck)
-            secondary.append(deck[j])
-
-            # score partner hand
-            if deck[j][0] == 1 : secondaryScore += 4
-            if deck[j][0] == 13 : secondaryScore += 3
-            if deck[j][0] == 12 : secondaryScore += 2
-            if deck[j][0] == 11 : secondaryScore += 1
-
-            # count number of suits in partner hand
-            if deck[j][1] == "Clubs" : distr[0] += 1
-            if deck[j][1] == "Spades" : distr[1] += 1
-            if deck[j][1] == "Diamonds" : distr[2] += 1
-            if deck[j][1] == "Hearts" : distr[3] += 1
-
-        # score suits for partner
-        size = len(distr)
-        for k in range(size):
-            if distr[k] == 0: secondaryScore += 5
-            if distr[k] == 1: secondaryScore += 2
-            if distr[k] == 2: secondaryScore += 1
+        # creates partner hand and evaluates
+        random.shuffle(deck)
+        secondary = deck[:13]
+        secondaryScore = evaluation(secondary)
 
         #RESULTS
-        #print(i, "  P.Score = ", partnerScore, "  T.Score = ", partnerScore + score)
         total = score + secondaryScore
         if (total < 20) : outcome[0] += 1
         if (19 < total < 26) : outcome[1] += 1
@@ -156,8 +75,7 @@ while play:
 
     # loop coverts from raw data to percent chance
     size = len(outcome)
-    for i in range(size):
-        outcome[i] = (outcome[i]/totalHands) * 100
+    for i in range(size): outcome[i] = (outcome[i]/totalHands) * 100
 
     print("\nThe estimated probability based on", totalHands ,"simulated hands:")
     print("Pass: ", outcome[0], '%')
@@ -166,6 +84,6 @@ while play:
     print("Small Slam: ", outcome[3], '%')
     print("Grand Slam: ", outcome[4], '%')
 
-    play = input("Would you like to play another hands? (true/false) ")
+    play = input("\nWould you like to play another hands? (true/false) ")
 
 
